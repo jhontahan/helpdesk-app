6c928a0dcd18e84585cdcd7363979c9297a67a9b
jhonathan Cruz <jhonathancruz48@gmail.com>
2022-12-10 02:25:23 -0300

    Adição de mensagem caso token expire

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService, 
+            private router: Router,
+            private toast: ToastrService){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -19,6 +22,8 @@ export class AuthGuard implements CanActivate {
     }
     else{
       this.router.navigate(['login']);
+      this.toast.info('Logue novamente para acessar o sistema.', 'Logout', {timeOut: 10000})
+
       return false;
     }
   }
